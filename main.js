a7240e73ce37cd64f1b8c22f7ace4973fc499933
Ivan Minaev <minaev70ivan009@gmail.com>
2023-12-11 23:50:16 +0300

    I have completed the mobile menu

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 const dropMenu = document.querySelector(".drop-menu");
 const menuBurger = document.querySelector(".menu-burger");
+const row = document.querySelectorAll(".row");
 menuBurger.addEventListener("click", (e) => {
     dropMenu.classList.toggle("hide");
+    row.forEach((el) => {
+        el.classList.toggle("hide");
+    });
+    if (!dropMenu.closest(".hide")) {
+        menuBurger.style = "position: fixed; right:20px";
+    } else {
+        menuBurger.style = "position: relative; right:none";
+    }
 });
